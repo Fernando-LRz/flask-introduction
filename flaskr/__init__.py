da16ee8469064d317407a02dcdcce63f32e05e48
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-03 12:42:51 -0600

    complete tutorial app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,14 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    
+    app.add_url_rule('/', endpoint='index')
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
